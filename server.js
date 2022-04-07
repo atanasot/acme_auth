4f7150e6b83379b08f2b633afa825ada7736efb9
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-07 14:42:36 -0400

    added all files

---
+++ server.js
@@ -0,0 +1,12 @@
+// starts server and seeds the db
+
+const { syncAndSeed } =  require('./db');
+const app = require('./app');
+
+const init = async()=> {
+  await syncAndSeed();
+  const port = process.env.PORT || 8080;
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
+};
+
+init();
